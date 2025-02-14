f223aff407abe0a368db650a82ce2201897afbe2
Danilean <danilo16formanski@gmail.com>
2025-02-14 16:04:13 -0300

    FIX: ajustando bugs encontrados

--- SC/driver_setup.py
+++ SC/driver_setup.py
@@ -1,22 +1,33 @@
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+
+load_dotenv()
 
 def iniciar_driver():
+    pasta_origem = os.getenv("PASTA_ORIGEM", r"C:\Users\danilo.formanski\Downloads\Arquivos-TCE")
+
     chrome_options = Options()
+    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--ignore-ssl-errors')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
     prefs = {
-        "download.default_directory": "C:/Users/danil/Code/Automacao-TCE-Selenium/SC/",
+        "download.default_directory": pasta_origem,
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     }
     chrome_options.add_experimental_option("prefs", prefs)
 
-    service = Service(ChromeDriverManager().install())
+    service = Service(r"C:\Users\danilo.formanski\Downloads\Code\Automacao-TCE-Selenium\SC\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.maximize_window()
+
     return driver
